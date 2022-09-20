e4a010c0acee46f97b59630cceff9963f9ad2079
Mahin12 <mahinarafatmeem@gmail.com>
2022-09-20 13:23:40 -0400

    Data science class practice which went better

--- hello/boston.py
+++ hello/boston.py
@@ -1,15 +1,14 @@
-from sklearn.model_selection import sklearn
 from cgi import test
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-dataset=pd.read_csv('https://forge.scilab.org/index.php/p/rdataset/source/tree/master/csv/MASS/Boston.csv')
+dataset = pd.read_csv('/Users/mahin/Downloads/Boston.csv')
 
 x=dataset.drop(['Unnamed: 0', 'medv'], axis=1)
 y=dataset['medv']
 
+print(dataset)
  
-import train_test_split
+# import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
+# x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
