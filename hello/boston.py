35d0a418269b7b8e9cc34f592df14297078d95f9
Mahin12 <mahinarafatmeem@gmail.com>
2022-09-26 18:33:53 -0400

    maxSubarray

--- hello/boston.py
+++ hello/boston.py
@@ -1,14 +1,13 @@
+from sklearn.model_selection import train_test_split
 from cgi import test
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('/Users/mahin/Downloads/Boston.csv')
 
-x=dataset.drop(['Unnamed: 0', 'medv'], axis=1)
-y=dataset['medv']
+x = dataset.drop(['Unnamed: 0', 'medv'], axis=1)
+y = dataset['medv']
 
-print(dataset)
- 
-# import train_test_split
 
-# x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
